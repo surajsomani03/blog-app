3fc61c38bc398b0bec5165fe90e97f9ca9141692
Suraj Somani <surajsomani2002@gmail.com>
2023-12-22 19:39:31 +0530

    CSS dark mode toggle design

--- src/components/themeToggle/ThemeToggle.jsx
+++ src/components/themeToggle/ThemeToggle.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import styles from "./themeToggle.module.css"
+import Image from 'next/image'
 const ThemeToggle = () => {
   return (
-    <div className={styles.container}>ThemeToggle</div>
+    <div className={styles.container}>
+      <Image src="/moon.png" alt='' height={14} width={14} />
+      <div className={styles.ball}></div>
+      <Image src="/sun.png" alt='' height={14} width={14} />
+    </div>
   )
 }
 
