2273668017962b3be69aa3eaa142f6aedffcd4b1
Suraj Somani <surajsomani2002@gmail.com>
2023-12-22 19:32:55 +0530

    responsive navbar design

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
 import styles from "./navbar.module.css"
+import Image from 'next/image'
+import Link from 'next/link'
+import ThemeToggle from '../themeToggle/ThemeToggle'
+import AuthLinks from '../authLinks/AuthLinks'
 const Navbar = () => {
   return (
-    <div className={styles.container}>Navbar</div>
+    <div className={styles.container}>
+      <div className={styles.social}>
+        <Image src='/facebook.png' alt='facebook' height={24} width={24} />
+        <Image src='/instagram.png' alt='instagram' height={24} width={24} />
+        <Image src='/tiktok.png' alt='tiktok' height={24} width={24} />
+        <Image src='/youtube.png' alt='youtube' height={24} width={24} />
+      </div>
+      <div className={styles.logo}>Crazyblog</div>
+      <div className={styles.links}>
+        <ThemeToggle/>
+        <Link href="/">Homepage</Link>
+        <Link href="/">Contact</Link>
+        <Link href="/">About</Link>
+        <AuthLinks/>
+      </div>
+    </div>
   )
 }
 
---
+++ src/components/themeToggle/ThemeToggle.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import styles from "./themeToggle.module.css"
+const ThemeToggle = () => {
+  return (
+    <div className={styles.container}>ThemeToggle</div>
+  )
+}
+
+export default ThemeToggle
