a86f6ead828d7c0ccb533ab6fc2a8183399a3859
Suraj Somani <surajsomani2002@gmail.com>
2023-12-22 15:38:02 +0530

    first commit css layout

---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import styles from "./homepage.module.css";
+
+export default function Home() {
+  return <div>
+    <h1>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis doloribus ipsa architecto temporibus assumenda numquam minima, deserunt debitis natus id quam facilis eligendi incidunt, optio ducimus dicta aspernatur laborum fugiat?
+    </h1>
+    <Link href="/">Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, sunt quis, nihil amet eligendi ullam dignissimos tenetur animi inventore deserunt molestias a laudantium aliquid temporibus quam, dicta ab possimus beatae.Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, sunt quis, nihil amet eligendi ullam dignissimos tenetur animi inventore deserunt molestias a laudantium aliquid temporibus quam, dicta ab possimus beatae.Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, sunt quis, nihil amet eligendi ullam dignissimos tenetur animi inventore deserunt molestias a laudantium aliquid temporibus quam, dicta ab possimus beatae.Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, sunt quis, nihil amet eligendi ullam dignissimos tenetur animi inventore deserunt molestias a laudantium aliquid temporibus quam, dicta ab possimus beatae.Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, sunt quis, nihil amet eligendi ullam dignissimos tenetur animi inventore deserunt molestias a laudantium aliquid temporibus quam, dicta ab possimus beatae.</Link>
+  </div>;
+}
